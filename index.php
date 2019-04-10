393e9666b7960bae4df7f600cb5cd3b5a2d8115c
M. Folkers <hello@twobrain.io>
2019-04-10 19:39:30 +0200

    Merging results into a single template file .. also updating pcbis2pdf to v0.4

--- index.php
+++ index.php
@@ -4,54 +4,45 @@ require_once('vendor/autoload.php');
 
 $currentIssue = '2019_01';
 
-function csv($issue)
-{
-    $object = new PCBIS2PDF\PCBIS2PDF;
-    $csv_raw = glob('./src/' . $issue . '/csv/*.csv');
 
-    foreach ($csv_raw as $file) {
+function generateIssue($issue)
+{
+    $object = new PCBIS2PDF\PCBIS2PDF('dist/' . $issue . '/images');
+
+    $src      = './src/' . $issue;
+    $dist     = './dist/' . $issue;
+
+    foreach (glob($src . '/csv/*.csv') as $file) {
         $fromCSV = $object->CSV2PHP($file);
-        $arrayFile = $object->process($fromCSV);
-
-        $outputData = [];
-
-        foreach ($arrayFile as $array) {
-            $category = basename($file, '.csv');
-            $array = a::update($array, ['Kategorie' => $category]);
-
-            $outputData[] = $array;
-        }
-
-        $object->PHP2CSV($outputData, './dist/' . $issue . '/csv/' . basename($file));
+        $data = $object->process($fromCSV);
+        $object->PHP2CSV($data, $dist . '/csv/' . basename($file));
     }
+
+    /**************************************************************************/
+
+    $dataFile = $dist . '/data.csv';
+    $object->mergeCSV(glob($dist . '/csv/*.csv'), $dataFile, true);
+
+    /**************************************************************************/
+
+    $template = $src . '/templates/dataList.sla';
+
+    if (file_exists($template)) {
+        $command = [
+            './vendor/berteh/scribusgenerator/ScribusGeneratorCLI.py', // Python script
+            '--single', // Single file output
+            '-c ' . $dataFile, // CSV file
+            '-d ";"', // Semicolon as delimiter
+            '-o ' . $dist . '/templates', // Output path
+            '-n ' . basename($template, '.sla'), // Output filename (without extension)
+            $template, // Template path
+        ];
+
+        exec(implode(' ', $command), $result);
+        a::show($result);
+    }
+
     return;
 }
 
-
-function scribus($issue)
-{
-    $files = glob('./dist/' . $issue . '/csv/*.csv');
-
-    foreach ($files as $file) {
-        $template = './src/'. $issue . '/templates/' . basename($file, '.csv') . '.sla';
-
-        if (file_exists($template)) {
-            $command = [
-                './vendor/berteh/scribusgenerator/ScribusGeneratorCLI.py', // Python script
-                '--single', // Single file output
-                '-c ' . $file, // CSV file
-                '-d ";"', // Semicolon as delimiter
-                '-o dist/' . $issue . '/templates', // Output path
-                '-n ' . basename($file, '.csv'), // Output filename (without extension)
-                $template, // Template path
-            ];
-
-            exec(implode(' ', $command), $result);
-            print_r($result);
-        }
-    }
-    return;
-}
-
-csv($currentIssue);
-scribus($currentIssue);
+generateIssue($currentIssue);
