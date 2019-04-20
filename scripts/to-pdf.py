cea801daec2514e75caee2ecc692259b8853f067
M. Folkers <hello@twobrain.io>
2019-04-20 19:05:38 +0200

    Adding useful scripts to separate folder

---
+++ scripts/to-pdf.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#
+# Produces a PDF for the SLA passed as a parameter.
+# Uses the same file name and replaces the .sla extension with .pdf
+#
+# usage:
+# scribus -g -py to-pdf.py -- file.sla
+#
+# license:
+# (c) MIT Ale Rimoldi
+
+import os
+import scribus
+
+if scribus.haveDoc() :
+    filename = os.path.splitext(scribus.getDocName())[0]
+    pdf = scribus.PDFfile()
+    pdf.file = filename+".pdf"
+    pdf.save()
+else :
+    print("No file open")
